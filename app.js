c90576f5ab8cd6c0972ad179305e4dccc799da41
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-09-19 21:47:14 +0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,204 @@
+// If statement tasks
+
+// Task 1
+// var a = prompt("city name ")
+// if ( a==="karachi")
+// {
+//     alert("welcome to city of lights")
+// }
+
+
+
+// Task 2
+// var a =prompt("enter your gender")
+// if (a==="male"){
+//     alert("hello sir")
+// }
+// if (a==="female"){
+//     alert("hello mam")
+// }
+// task 3
+// var a = prompt("Enter traffic light color")
+// if (a === "red"){
+//     alert("It means stop")
+// }
+// if (a=== "green"){
+//     alert("It means go")
+//     if (a=== "yellow"){
+//         alert("It means start your vehicle")
+//     }
+// }
+
+// task 4
+// var a = prompt("write your remainig fuel in your car")
+// if (a<=0.25+""+"litre")
+// {
+//     alert ("refill it")
+// }
+
+
+// task 5
+// true
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+
+// var b = 82;
+// // false
+// if (b++ === 83){
+//     // console.log(b)
+// alert("given condition for variable b is true");
+// }
+
+
+// var c = 12;
+// // false
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// // true
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// // false
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// // true
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// true
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+
+// if (true){
+//     alert("True");
+//     }
+
+
+//     // agr condition mai false hoga toh woh pront hi nahi karega
+//     if (false){
+//     alert("False");
+//     }
+
+
+    // if("car" < "elephant"){
+    //     alert("car is smaller than elephant");
+    //     }
+
+
+
+
+//     var total = +prompt("Total 3 subjects marks")
+//     document.write("Total marks : "+ total +"<br>")
+//     var eng = +prompt("english obtained marks")
+
+//     var comp = +prompt("computer obtained marks")
+//     var sst = +prompt("sst obtained marks")
+//    var total_obtained_marks = eng+comp+sst
+//    document.write("Total marks "+ total_obtained_marks +"<br>")
+// var per = total_obtained_marks/total*100
+// document.write("Percentage : "+ per +"<br>")
+// if (per >= 80){
+//     document.write("Grade : a-one "+ " <br>")
+//     document.write("Remarks : excellent")
+// }
+// if (per >= 70){
+//     document.write("Grade : A "+ " <br>")
+//     document.write("Remarks : Good")
+// }
+// if (per >= 60){
+//     document.write("Grade : B "+ " <br>")
+//     document.write("Remarks : You need to improve")
+// }
+// if (per < 60){
+//     document.write("Grade : Fail "+ " <br>")
+//     document.write("Remarks : sorry")
+// }
+
+// task 7
+// var a = 4
+// var b = +prompt("Guess the secret number")
+// if(b === a )
+// {
+//     alert("Bingo! Correct answer")
+// }
+// if(b===3){
+//     alert("close ")
+// }
+// if(b===5){
+//     alert("close ")
+// }
+
+// task 8
+// var a = prompt("Enter any number")
+// if(a % 3 === 0 ) {
+//     alert("divisible by 3")
+// }
+
+
+// task 9
+// var a = 2
+// var b = 3
+// var  c = prompt("enter any number")
+// if(c % 2 === 0)
+// {
+//     alert("given num is even ")
+// }
+// if(c % 2 !== 0 )
+// {
+//     alert("given num is odd ")
+// }
+
+
+// task 10
+// var a = prompt('Write your city temperature')
+// if(a > 40 ){
+//     alert('It is too hot outside.')
+// }
+// if(a > 30 ){
+//     alert('The Weather today is Normal.')
+// }
+// if(a > 20 ){
+//     alert('Today’s Weather is cool.')
+// }
+// if(a > 10 ){
+//     alert('OMG! Today’s weather is so Cool.')
+// }
+
+
+// task 11
+// var a =  prompt("write 1st number")
+// var b = prompt("write 2nd number ")
+// var o = prompt("write any opertor(+, -, *, /, %)")
+// if (o === "+")
+// {
+//     alert(+a + +b)
+// }
+// if (o === "-")
+// {
+//     alert(a-b)
+// }
+
+// if (o === "/")
+// {
+//     alert(a/b)
+// }
+
+// if (o === "%")
+// {
+//     alert(a%b)
+// }
+// if (o === "*")
+// {
+//     alert(a*b)
+}
